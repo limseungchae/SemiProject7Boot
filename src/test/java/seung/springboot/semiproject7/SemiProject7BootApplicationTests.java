cc86d0cec011d761c5cf4d9ad9736aa182b26b06
limseungchae <tmdco12@naver.com>
2023-05-18 15:11:30 +0900

    프로젝트 초기화

---
+++ src/test/java/seung/springboot/semiproject7/SemiProject7BootApplicationTests.java
@@ -0,0 +1,13 @@
+package seung.springboot.semiproject7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemiProject7BootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
