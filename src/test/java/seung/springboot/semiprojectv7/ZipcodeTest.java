4937bfdc31bbaa2fcc71bb3af042d776555dc55e
limseungchae <tmdco12@naver.com>
2023-05-19 16:38:25 +0900

    우편번호 like 검색 및 페이징 테스트

--- src/test/java/seung/springboot/semiprojectv7/ZipcodeTest.java
+++ src/test/java/seung/springboot/semiprojectv7/ZipcodeTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import seung.springboot.semiprojectv7.model.Zipcode;
 import seung.springboot.semiprojectv7.repository.ZipcodeRepository;
 
@@ -22,5 +25,29 @@ public class ZipcodeTest {
         List<Zipcode> addr = zipcodeRepository.findZipcodeByDong("구로");
 
         System.out.println(addr);
+    }
+
+    @Test
+    @DisplayName("zipcode page1")
+    public void pageZipcode1() {
+
+        Pageable pageable = PageRequest.of(1, 15);  // 전체 페이지
+        Page<Zipcode> page = zipcodeRepository.findAll(pageable);
+        List<Zipcode> zips = page.getContent();
+
+
+        System.out.println(zips);
+    }
+
+    @Test
+    @DisplayName("zipcode page2")
+    public void pageZipcode2() {
+        String dong = "%" + "구로" + "%";
+        Pageable pageable = PageRequest.of(1, 15);  // 1 페이지
+        Page<Zipcode> page = zipcodeRepository.findByDongLike(dong, pageable);
+        List<Zipcode> zips = page.getContent();
+
+
+        System.out.println(zips);
     }
 }
