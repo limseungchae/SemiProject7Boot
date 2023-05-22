09676efd7b3a899c712dac5fa68d9e38a15b1ab9
limseungchae <tmdco12@naver.com>
2023-05-22 11:50:50 +0900

    쿼리 메서드를 이용한 다중 검색기능 구현 3 (like) - contains

--- src/main/java/seung/springboot/semiprojectv7/repository/BoardRepository.java
+++ src/main/java/seung/springboot/semiprojectv7/repository/BoardRepository.java
@@ -24,8 +24,8 @@ public interface BoardRepository extends PagingAndSortingRepository<Board, Long>
     //@Query("select ceil(count(bno)/25) from Board")
     int countBoardBy();
 
-    List<Board> findByTitleLike(Pageable paging, String fkey);
-    List<Board> findByTitleLikeOrContentLike(Pageable paging, String fkey1, String fkey2);
+    List<Board> findByTitleContains(Pageable paging, String fkey);
+    List<Board> findByTitleContainsOrContentContains(Pageable paging, String fkey1, String fkey2);
     List<Board> findByUserid(Pageable paging, String fkey);
-    List<Board> findByContentLike(Pageable paging, String fkey);
+    List<Board> findByContentContains(Pageable paging, String fkey);
 }
