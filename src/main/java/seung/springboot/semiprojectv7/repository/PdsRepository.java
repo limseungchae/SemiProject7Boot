102953176a0f64886b2d498be0d71849f76c7ff3
limseungchae <tmdco12@naver.com>
2023-05-24 18:17:00 +0900

    자료실 - Repository 구현

---
+++ src/main/java/seung/springboot/semiprojectv7/repository/PdsRepository.java
@@ -0,0 +1,7 @@
+package seung.springboot.semiprojectv7.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import seung.springboot.semiprojectv7.model.Pds;
+
+public interface PdsRepository extends JpaRepository<Pds, Long> {
+}
