49e3e63ea7b630a08b877125786f38ee6fad77c2
limseungchae <tmdco12@naver.com>
2023-05-25 11:55:20 +0900

    자료실 - 목록보기 dso 구현

--- src/main/java/seung/springboot/semiprojectv7/dao/PdsDAO.java
+++ src/main/java/seung/springboot/semiprojectv7/dao/PdsDAO.java
@@ -3,9 +3,13 @@ package seung.springboot.semiprojectv7.dao;
 import seung.springboot.semiprojectv7.model.Pds;
 import seung.springboot.semiprojectv7.model.PdsAttach;
 
+import java.util.Map;
+
 public interface PdsDAO {
 
     int insertPda(Pds pds);
 
     int insertPdsAttach(PdsAttach pa);
+
+    Map<String, Object> selectPds(int cpg);
 }
