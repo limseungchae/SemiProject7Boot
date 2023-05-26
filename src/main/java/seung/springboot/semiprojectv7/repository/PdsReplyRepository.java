5a3e36bfedb65c6edf7ba8f94c839faa45e27de3
limseungchae <tmdco12@naver.com>
2023-05-26 16:15:07 +0900

    자료실 댓글 - 댓글쓰기 repository 구현

--- src/main/java/seung/springboot/semiprojectv7/repository/PdsReplyRepository.java
+++ src/main/java/seung/springboot/semiprojectv7/repository/PdsReplyRepository.java
@@ -1,10 +1,12 @@
 package seung.springboot.semiprojectv7.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import seung.springboot.semiprojectv7.model.PdsReply;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface PdsReplyRepository extends JpaRepository<PdsReply, Long> {
@@ -12,4 +14,8 @@ public interface PdsReplyRepository extends JpaRepository<PdsReply, Long> {
     // @Query("from PdsReply  where pno = :pno order by refno asc")
     List<PdsReply> findByPnoOrderByRefnoAscRegdateAsc(int pno);
 
+    @Transactional
+    @Modifying
+    @Query("update PdsReply set refno = :rpno where rpno = :rpno")
+    void updateRefno(@Param("rpno") int rpno);
 }
