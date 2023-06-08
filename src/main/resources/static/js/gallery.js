16a184ec400a87cecb90fe6de49007e99eb0892a
limseungchae <tmdco12@naver.com>
2023-06-08 10:58:38 +0900

    갤러리 - gallery 조건검사 수정

--- src/main/resources/static/js/gallery.js
+++ src/main/resources/static/js/gallery.js
@@ -1,27 +1,26 @@
 // 이미지 첨부 조건 검사
 const checkAttachs = () => {
-    let checkOK = true;
+    let checkOk = true;
     const attachs = document.querySelector("#attachs");
 
-    // 이미지 첨부파일 하나 이상이라면
+    // 이미지 첨부파일이 하나 이상이라면
     if ('files' in attachs && attachs.files.length > 0) {
         // 첨부파일들이 이미지인지 검사
         for (attach of attachs.files) {
-            // console.log(attach.name + ',' + attach.type + ',' + attach.size);
+            //console.log(attach.name + ',' + attach.type + ',' + attach.size);
             // 이미지 파일의 MIME 형식
-            // image/jpg, image/jpeg, image/png, image/gif
+            // image/jpg,image/jpeg,image/png,image/gif
             if (!attach.type.startsWith('image')) {
                 alert('첨부하려는 파일은 반드시 이미지여야 합니다!');
-                checkOK = false;
+                checkOk = false;
             }
         }
     } else {
         alert('하나 이상의 이미지를 선택하세요!!');
-        checkOK = false;
+        checkOk = false;
     }
 
-
-    return false;
+    return checkOk;
 };
 
 // 새글쓰기
@@ -30,7 +29,7 @@ writebtn?.addEventListener('click', () => {
     const galfrm = document.forms.galfrm;
     if (galfrm.title.value === '') alert('제목을 작성하세요!');
     else if (galfrm.content.value === '') alert('본문을 작성하세요!');
-    else if (!checkAttachs()) alert('이미지 첨부시 조건 불일치!!');
+    else if (!checkAttachs()) alert('이미지 첨부 조건 불일치!!');
     else if (grecaptcha.getResponse() === '') alert('자동가입방지를 확인하세요!');
     else {
         galfrm.method = 'post';
@@ -41,9 +40,8 @@ writebtn?.addEventListener('click', () => {
 
 // 목록보기
 // 새글쓰기 버튼
-const newbtn = document.querySelector("#newbtn")
+const newbtn = document.querySelector("#newbtn");
 newbtn?.addEventListener('click', () => {
     location.href = '/gallery/write';
 });
-
 // 본문보기
