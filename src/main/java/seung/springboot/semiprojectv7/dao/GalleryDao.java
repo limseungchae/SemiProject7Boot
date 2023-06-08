993148dd4c9fd89bcab1a55748d41abe9b9b78ae
limseungchae <tmdco12@naver.com>
2023-06-08 12:07:39 +0900

    갤러리 - gallery list dao

--- src/main/java/seung/springboot/semiprojectv7/dao/GalleryDao.java
+++ src/main/java/seung/springboot/semiprojectv7/dao/GalleryDao.java
@@ -3,8 +3,12 @@ package seung.springboot.semiprojectv7.dao;
 import seung.springboot.semiprojectv7.model.GalAttach;
 import seung.springboot.semiprojectv7.model.Gallery;
 
+import java.util.Map;
+
 public interface GalleryDao {
     int insertGal(Gallery gallery);
 
     int insertGalAttach(GalAttach ga);
+
+    Map<String, Object> selectGallery(Integer cpg);
 }
--- src/main/java/seung/springboot/semiprojectv7/dao/GalleryDaoImpl.java
+++ src/main/java/seung/springboot/semiprojectv7/dao/GalleryDaoImpl.java
@@ -1,11 +1,17 @@
 package seung.springboot.semiprojectv7.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 import seung.springboot.semiprojectv7.model.GalAttach;
 import seung.springboot.semiprojectv7.model.Gallery;
 import seung.springboot.semiprojectv7.repository.GalleryRepository;
 import seung.springboot.semiprojectv7.repository.GalleryaRepository;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Repository("galdao")
 public class GalleryDaoImpl implements GalleryDao{
@@ -28,5 +34,18 @@ public class GalleryDaoImpl implements GalleryDao{
     @Override
     public int insertGalAttach(GalAttach ga) {
         return galleryaRepository.save(ga).getGno();
+    }
+
+    @Override
+    public Map<String, Object> selectGallery(Integer cpg) {
+
+        Pageable paging = PageRequest.of(cpg, 25, Sort.Direction.DESC, "gno");
+
+        Map<String, Object> gals = new HashMap<>();
+        // Gallery와 GalAttach를 조인해서 리스트로 가져옴
+        gals.put("gallist", galleryaRepository.findAllBy(paging).getContent());
+        gals.put("cntpg", galleryaRepository.findAllBy(paging).getTotalPages());
+
+        return gals;
     }
 }
