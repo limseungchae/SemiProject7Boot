93f2c3e15624c944005151d92158d05a5fd9fe84
limseungchae <tmdco12@naver.com>
2023-06-08 10:55:26 +0900

    갤러리 - gallery daoImpl 수정

--- src/main/java/seung/springboot/semiprojectv7/dao/GalleryDaoImpl.java
+++ src/main/java/seung/springboot/semiprojectv7/dao/GalleryDaoImpl.java
@@ -1,19 +1,32 @@
 package seung.springboot.semiprojectv7.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import seung.springboot.semiprojectv7.model.GalAttach;
 import seung.springboot.semiprojectv7.model.Gallery;
+import seung.springboot.semiprojectv7.repository.GalleryRepository;
+import seung.springboot.semiprojectv7.repository.GalleryaRepository;
 
 @Repository("galdao")
 public class GalleryDaoImpl implements GalleryDao{
 
+    // 생성자를 이용한 스프링 빈 주입
+    private final GalleryRepository galleryRepository;
+    private final GalleryaRepository galleryaRepository;
+
+    @Autowired
+    public GalleryDaoImpl(GalleryRepository galleryRepository, GalleryaRepository galleryaRepository) {
+        this.galleryaRepository = galleryaRepository;
+        this.galleryRepository = galleryRepository;
+    }
+
     @Override
     public int insertGal(Gallery gallery) {
-        return 0;
+        return Math.toIntExact(galleryRepository.save(gallery).getGno());
     }
 
     @Override
     public int insertGalAttach(GalAttach ga) {
-        return 0;
+        return galleryaRepository.save(ga).getGno();
     }
 }
